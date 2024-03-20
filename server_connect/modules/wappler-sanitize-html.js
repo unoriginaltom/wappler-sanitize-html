961ea4c548acd5e4f4c4710c41517376654c8845
unoriginaltom <39840065+unoriginaltom@users.noreply.github.com>
2024-03-20 04:43:47 +0000

    Add files via upload
    Fix data bindings not working

--- server_connect/modules/wappler-sanitize-html.js
+++ server_connect/modules/wappler-sanitize-html.js
@@ -1,16 +1,25 @@
 const sanitizeHtml = require('sanitize-html');
 
-exports.sanitizeHTML = function(options, name) {
-  const { html, disallowedTagsMode, allowedTags, nonBooleanAttributes, allowedIframeHostnames, allowedIframeDomains, allowIframeRelativeUrls} = options;
-  
-  const cleanedHtml = sanitizeHtml(html, {
-    disallowedTagsMode: disallowedTagsMode,
-    allowedTags: allowedTags,
-    nonBooleanAttributes: nonBooleanAttributes,
-    allowedIframeHostnames: allowedIframeHostnames,
-    allowedIframeDomains: allowedIframeDomains,
-    allowIframeRelativeUrls: allowIframeRelativeUrls
-  });
-  
-  return cleanedHtml;
+exports.sanitizeHTML = function(options) {
+
+    const html = options.html ? this.parseRequired(options.html, 'string') : null;
+    const disallowedTagsMode = options.disallowedTagsMode ? this.parseRequired(options.disallowedTagsMode, 'string') : null;
+    const allowedTags = options.allowedTags ? this.parseRequired(options.allowedTags, 'string') : null;
+    const nonBooleanAttributes = options.nonBooleanAttributes ? this.parseRequired(options.nonBooleanAttributes, 'string') : null;
+    const allowedIframeHostnames = options.allowedIframeHostnames ? this.parseRequired(options.allowedIframeHostnames, 'string') : null;
+    const allowedIframeDomains = options.allowedIframeDomains ? this.parseRequired(options.allowedIframeDomains, 'string') : null;
+    const allowIframeRelativeUrls = options.allowIframeRelativeUrls ? this.parseRequired(options.allowIframeRelativeUrls, 'boolean') : null;
+
+    const sanitizeOptions = {
+        disallowedTagsMode,
+        allowedTags,
+        nonBooleanAttributes,
+        allowedIframeHostnames,
+        allowedIframeDomains,
+        allowIframeRelativeUrls
+    };
+
+    const cleanedHtml = sanitizeHtml(html, sanitizeOptions);
+
+    return cleanedHtml;
 };
