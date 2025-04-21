74d8548f52a53365d40a4a096ea1593e3f447460
unoriginaltom <39840065+unoriginaltom@users.noreply.github.com>
2025-04-20 22:42:26 -0500

    Update wappler-sanitize-html.js
    Add support for allowed attributes

--- server_connect/modules/wappler-sanitize-html.js
+++ server_connect/modules/wappler-sanitize-html.js
@@ -1,6 +1,6 @@
 const sanitizeHtml = require('sanitize-html');
 
-exports.sanitizeHTML = function(options) {
+exports.sanitizeHTML = function (options) {
 
     const html = options.html ? this.parseRequired(options.html, 'string') : null;
     const disallowedTagsMode = options.disallowedTagsMode ? this.parseRequired(options.disallowedTagsMode, 'string') : null;
@@ -10,9 +10,26 @@ exports.sanitizeHTML = function(options) {
     const allowedIframeDomains = options.allowedIframeDomains ? this.parseRequired(options.allowedIframeDomains, 'string') : null;
     const allowIframeRelativeUrls = options.allowIframeRelativeUrls ? this.parseRequired(options.allowIframeRelativeUrls, 'boolean') : null;
 
+    let allowedAttributes = {};
+    if (Array.isArray(options.allowedAttributes)) {
+        options.allowedAttributes.forEach(entry => {
+            const tag = entry.tag.trim();
+            const attrs = entry.attributes.split(',').map(attr => attr.trim()).filter(Boolean);
+            if (!allowedAttributes[tag]) {
+                allowedAttributes[tag] = [];
+            }
+            allowedAttributes[tag].push(...attrs);
+        });
+
+        for (const tag in allowedAttributes) {
+            allowedAttributes[tag] = [...new Set(allowedAttributes[tag])];
+        }
+    }
+
     const sanitizeOptions = {
         disallowedTagsMode,
         allowedTags,
+        allowedAttributes,
         nonBooleanAttributes,
         allowedIframeHostnames,
         allowedIframeDomains,
